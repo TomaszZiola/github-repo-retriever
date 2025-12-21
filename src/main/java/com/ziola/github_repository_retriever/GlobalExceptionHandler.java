00b3942bd19ee054a0f8dd1bb90f6edfe6e0c1fc
Tomasz Ziola <tomek.ziola@gmail.com>
2025-12-21 21:05:59 +0100

    Rename `ExceptionHandler` to `GlobalExceptionHandler` for consistency in naming conventions

--- src/main/java/com/ziola/github_repository_retriever/GlobalExceptionHandler.java
+++ src/main/java/com/ziola/github_repository_retriever/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
 package com.ziola.github_repository_retriever;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
-class ExceptionHandler {
+class GlobalExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(UserNotFoundException.class)
+    @ExceptionHandler(UserNotFoundException.class)
     ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
         var response = new ErrorResponse(NOT_FOUND.value(), ex.getMessage());
         return new ResponseEntity<>(response, NOT_FOUND);
